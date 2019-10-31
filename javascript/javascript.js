cff9b35a8d1c54dc5e3f16d9cada753c206c1c8b
Julius <juliushonee@gmail.com>
2019-10-31 14:24:34 +0100

    Eindopdracht
    Eindopdracht af.

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,5 +1,11 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console': 0*/
+
 var danceRadio = document.getElementById("dance");
 var form = document.getElementById("filter");
+
+
 
 danceRadio.addEventListener("click", function () {
     form.submit();
